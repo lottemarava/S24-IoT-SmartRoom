1058d7066efb8390f4d43ec1aa532eaf9c2083f5
sarahhh1 <84376361+sarahhh1@users.noreply.github.com>
2024-07-02 10:51:59 +0300

    Add files via upload

--- motion_function.h
+++ motion_function.h
@@ -1,17 +1,31 @@
+
 #ifndef MOTION_FUNCTION_H
 #define MOTION_FUNCTION_H
 
 #include <Arduino.h>
+#include <ld2410.h>
 #include "time_functions.h"
 #include "wifi_functions.h"
+#define LD24_INPUT 35
+extern ld2410 radar;
 
-#define PIRInput 34
-extern int pirState;
-extern int val;
 extern bool motion_detected;
 extern int offset;
+extern int  radarState;
 
-//detect motion and act according to result
+
+
+extern int lastSeen[20]; 
+extern int i ;
+extern const int size ;
+extern int sum ;
+extern int j;
+
+
+
+
+
+// Detect motion and act according to result
 void detectMotion();
 
 #endif
