10979b871245787da32c40dee0bdc7bac8d8aca2
lottemarava <lottemarava@gmail.com>
2024-07-16 10:06:54 +0300

    .\ESPandFireStore\*

--- UT/ESPandFireStore/test/secrets.h
+++ UT/ESPandFireStore/test/secrets.h
@@ -1,15 +1,15 @@
 /* 1. Define the WiFi credentials */
-#define WIFI_SSID ""
-#define WIFI_PASSWORD ""
+#define WIFI_SSID "Raz_and_Lottem"
+#define WIFI_PASSWORD "0527503502"
 
 // For the following credentials, see examples/Authentications/SignInAsUser/EmailPassword/EmailPassword.ino
 
 /* 2. Define the API Key */
-#define API_KEY ""
+#define API_KEY "AIzaSyDt7g0i9R7u7SJEt7S6q0Z-f6IC01h81KE"
 
 /* 3. Define the RTDB URL */
-#define DATABASE_URL "" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
+#define DATABASE_URL "https://smart-room-6eed5-default-rtdb.firebaseio.com" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
 
 /* 4. Define the user Email and password that alreadey registerd or added in your project */
-#define USER_EMAIL ""
-#define USER_PASSWORD ""
+#define USER_EMAIL "lottemarava@gmail.com"
+#define USER_PASSWORD "123789"
