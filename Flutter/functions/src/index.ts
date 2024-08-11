adaab7d19a767147b11baf4defd526616b4e518d
lottemarava <lottemarava@gmail.com>
2024-08-11 14:57:17 +0300

    track background notifications

---
+++ Flutter/functions/src/index.ts
@@ -0,0 +1,48 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+// export const helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import * as moment from "moment";
+
+admin.initializeApp();
+
+exports.sendNotification = functions.firestore
+  .document("Dates/{docId}")
+  .onUpdate((change, context) => {
+    const todayId = moment().format("YYYY-MM-DD");
+
+    const updatedDocId = context.params.docId;
+
+    if (updatedDocId === todayId) {
+      const payload = {
+        notification: {
+          title: "Night Light",
+          body: "A new activity was detected",
+        },
+      };
+      return admin.messaging().sendToTopic("all", payload)
+        .then((response) => {
+          console.log("Successfully sent message:", response);
+          return null;
+        })
+        .catch((error) => {
+          console.log("Error sending message:", error);
+        });
+    }
+    return null;
+  });
--- Flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,17 +6,25 @@ import FlutterMacOS
 import Foundation
 
 import cloud_firestore
+import firebase_auth
 import firebase_core
+import firebase_database
+import firebase_messaging
 import flutter_blue
 import flutter_blue_plus
+import flutter_local_notifications
 import path_provider_foundation
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterBluePlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlugin"))
   FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
