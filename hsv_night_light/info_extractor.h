2d101c893edd0f19cc8dd254cb6b273fcb97b0c8
lottemarava <lottemarava@gmail.com>
2024-08-11 17:24:49 +0300

    remove

--- hsv_night_light/info_extractor.h
+++
@@ -1,9 +0,0 @@
-#ifndef INFO_EXTRACTOR_H
-#define INFO_EXTRACTOR_H
-
-#include <Arduino.h>
-
-//dividing a merged string (of style "str1+str2+str3+...+strn") into its substrings and returning a particular substring
-String getValue(String data, char separator, int index);
-
-#endif
