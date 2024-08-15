e5fd43e6283e7c177ad803d931b1103f4fcfb0b3
lottemarava <lottemarava@gmail.com>
2024-08-15 17:33:50 +0300

    fix cloud function (already deployed)

--- Flutter/functions/src/index.ts
+++ Flutter/functions/src/index.ts
@@ -1,51 +1,15 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-import * as moment from "moment";
 
 admin.initializeApp();
 
 exports.sendNotification = functions.firestore
-  .document("woke_up_collection/{docId}")
-  .onUpdate((change, context) => {
-    const todayId = moment().format("YYYY-MM-DD");
-    const updatedDocId = context.params.docId;
-
-    if (updatedDocId === todayId) {
-      const payload = {
-        notification: {
-          title: "Night Light",
-          body: "A new activity was detected",
-        },
-      };
-      try {
-        admin.messaging().sendToTopic("all", payload)
-          .then((response) => {
-            console.log("Successfully sent message:", response);
-            return null;
-          })
-          .catch((error) => {
-            console.log("Error sending message:", error);
-          });
-      } catch (error) {
-        console.error("error sending message");
-      }
-    }
-    return null;
+  .document("woke_up_collection/{date_id}")
+  .onUpdate(async (change, context) => {
+    await admin.messaging().sendToTopic("NightLight", {
+      notification: {
+        title: "Night Light",
+        body: "Night Time Activity Detected",
+      },
+    });
   });
